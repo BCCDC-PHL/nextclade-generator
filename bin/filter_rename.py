2cbcb3e71abf6be38addfdb51ef07a4e9862c62a
jpalmer37 <john.palmer1288@gmail.com>
2023-08-21 08:28:50 -0700

    updates to filtering rename script

--- bin/filter_rename.py
+++ bin/filter_rename.py
@@ -4,28 +4,38 @@ import re
 
 seqs = list(SeqIO.parse(sys.argv[1], 'fasta'))
 
-unique = set()
-temp = []
-for seq in seqs:
-	if seq.id not in unique:
-		unique.add(seq.id)
-		temp.append(seq)
+def filter_unique(seq_list):
+	unique = set()
+	filtered = []
+	for seq in seq_list:
+		if seq.id not in unique:
+			unique.add(seq.id)
+			filtered.append(seq)
 
-seqs = temp
-
-seqs = [x for x in seqs if x.seq.count("N") / len(x) < 0.1]
-
-newseqs = []
-for x in seqs:
-	search1 = re.search("([^\s]+)[^\(]*\(([^\)]+\))", x.description)
-	search2 = re.search('(H\dN\d)', x.description)
-	if search1 and search2:
-		x.id = "|".join(search1.groups()).replace(" ","_") + "|A|" + search2.group(1) 
-		x.name = x.id
-		x.description = ''
-		newseqs.append(x)
-
-newseqs = [x for x in newseqs if "|A/" in x.id]
+	return filtered
 
 
-SeqIO.write(newseqs, sys.argv[2], 'fasta')
+def filter_N(seq_list):
+	return [x for x in seq_list if x.seq.count("N") / len(x) < 0.1]
+
+def rename_headers(seq_list):
+	newseqs = []
+	for x in seq_list:
+		re_fields = re.search("([^\s]+)[^\(]*\(([^\)]+\))", x.description)
+		re_subtype = re.search('(H\dN\d)', x.description)
+
+		if re_fields and re_subtype:
+			x.id = "|".join(re_fields.groups()).replace(" ","_") + "|A|" + re_subtype.group(1) 
+			x.name = x.id
+			x.description = ''
+			newseqs.append(x)
+	
+	return [x for x in newseqs if "|A/" in x.id]
+
+
+seqs = filter_unique(seqs)
+seqs = filter_N(seqs)
+seqs = rename_headers(seqs)
+
+
+SeqIO.write(seqs, sys.argv[2], 'fasta')
