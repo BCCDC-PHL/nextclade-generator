ba304d32ad90e27e0915444a5da3d16b9179862a
jpalmer37 <john.palmer1288@gmail.com>
2023-10-03 10:48:01 -0700

    updated compute_mutations.py toolkit

--- bin/compute_mutations.py
+++ bin/compute_mutations.py
@@ -3,7 +3,7 @@ import os, sys
 from Bio import SeqIO
 import argparse
 import multiprocessing as mp
-from tools import pairwise_alignment, flex_translate, init_aligner
+from tools import pairwise_alignment, flex_translate, init_aligner, get_boundaries
 
 os.chdir(os.path.dirname(__file__))
 
@@ -16,11 +16,24 @@ def init_parser():
 	parser.add_argument("-t",'--tree', required=True, help='Path to a metadata describing all samples participating in the dataset')
 	parser.add_argument("-n",'--threads', default=1, type=int, help='Number of threads to use when computing mutations.')
 
-def get_nucl_mutations(aligner, ref, qry):
+def pairwise_alignment(aligner, ref, qry):
+
+	ref_aln, qry_aln = next(aligner.align(ref, qry))
+	# cut down the alignment to only the region of interest (that which overlaps with reference)
+	start, stop = get_boundaries(ref_aln)
+	ref_aln = ref_aln[start:stop]
+	qry_aln = qry_aln[start:stop]
+	return ref_aln, qry_aln
+
+def nucl_align(aligner, ref, qry):
+
 	ref_aln, qry_aln = pairwise_alignment(aligner, ref, qry)
-	
+
 	mut_list = []
-	for n, (char1, char2) in enumerate(zip(ref_aln, qry_aln),1):
+
+	combined = [(c1, c2) for c1, c2 in zip(ref_aln, qry_aln) if c1 != '-']
+
+	for n, (char1, char2) in enumerate(combined, 1):
 		if char1 != char2 and char1 not in ['*','-','?'] and char2 not in ['*','-','?']:
 			mut_list.append((int(n), char2))
 	return mut_list
@@ -32,20 +45,23 @@ def transform(pos):
 	elif pos > 23:
 		return "HA1", pos - 23
 	elif pos > 7:
-		return "SigPep", pos - 7
+		return "ERROR", pos - 7
 	else:
 		return "ERROR", -1
 
-def get_aa_mutations(aligner, aa_ref, nt_qry):
+def aa_align(aligner, aa_ref, nt_qry):
+
 	aa_qry, _, _ = flex_translate(nt_qry)
 
 	ref_aln, qry_aln = pairwise_alignment(aligner, aa_ref, aa_qry)
 
+	combined = [(c1, c2) for c1, c2 in zip(ref_aln, qry_aln) if c1 != '-']
+
 	mut_list = []
-	for n, (char1, char2) in enumerate(zip(ref_aln, qry_aln),1):
+	for n, (char1, char2) in enumerate(combined, 1):
 		if char1 != char2 and char1 not in ['*','-','?'] and char2 not in ['*','-','?']:
 			gene, position = transform(n)
-			if gene != "ERROR" and gene != "SigPep":
+			if gene != "ERROR":
 				mut_list.append((gene, int(position), char2))
 
 	return mut_list
@@ -55,9 +71,10 @@ def get_all_mutations(ref_path, qry_path, nthreads):
 
 	nuc_ref = next(SeqIO.parse(ref_path,'fasta'))
 	
-
 	aa_ref = nuc_ref.translate()
+
 	nuc_ref = str(nuc_ref.seq)
+	aa_ref = str(aa_ref.seq)
 
 	seqs = list(SeqIO.parse(qry_path, 'fasta'))
 	aligner = init_aligner()
@@ -65,13 +82,12 @@ def get_all_mutations(ref_path, qry_path, nthreads):
 	global nucl_align_wrap    		# necessary to run function in parallel
 
 	def nucl_align_wrap(x):
-		return get_nucl_mutations(aligner, nuc_ref, str(x.seq))
-	
+		return nucl_align(aligner, nuc_ref, str(x.seq))
 
 	global aa_align_wrap 			# necessary to run function in parallel
 
 	def aa_align_wrap(x):
-		return get_aa_mutations(aligner, aa_ref, str(x.seq))
+		return aa_align(aligner, aa_ref, str(x.seq))
 
 
 	print("Starting parallelized.")
